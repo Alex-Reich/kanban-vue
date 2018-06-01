f0f3b3bdcb6818c1cf2ff6b8daf5885cb3d4c970
Alex Reich <atrain.reich@gmail.com>
2018-06-01 14:39:30 -0600

    More heroku tweaks

--- server/index.js
+++ server/index.js
@@ -1,15 +1,15 @@
 var express = require('express')
 var bp = require('body-parser')
 var app = express()
+var server = require('http').createServer(app)
 var cors = require('cors')
 var port = process.env.PORT || 3000
 
-app.use(express.static(__dirname + '../www/dist'))
 
 var whitelist = ['http://localhost:8080', 'http://kanban--vue.herokuapp.com'];
 var corsOptions = {
-	origin: function (origin, callback) {
-		var originIsWhitelisted = whitelist.indexOf(origin) !== -1;
+  origin: function (origin, callback) {
+    var originIsWhitelisted = whitelist.indexOf(origin) !== -1;
 		callback(null, originIsWhitelisted);
 	},
 	credentials: true
@@ -19,6 +19,7 @@ app.use(cors(corsOptions))
 //Fire up database connection
 require('./server-assets/db/mlab-config')
 
+app.use(express.static(__dirname + '/../www/dist'))
 
 //REGISTER MIDDLEWEAR
 app.use(bp.json())
